75389a2dabf7bd6f84d3381364e9c91e18cbdf91
KuangLong <easoneasonapple@gmail.com>
2024-12-12 14:55:40 +0800

    update

--- gdino_pro_main.py
+++ gdino_pro_main.py
@@ -7,6 +7,9 @@ from lang_sam import BoundingBoxSAM
 def main(inp, image_path, image_url):
     # load image
     image = Image.open(image_path).convert('RGB')
+    
+    image_name = str(image_path).split('/')
+    image_name = image_name[len(image_name) - 1]
 
     # initialize models
     llava = Llava()
@@ -14,34 +17,45 @@ def main(inp, image_path, image_url):
     qd = QuestionDetector()
 
     # Generate image description with Llava
-    description = llava.chat(image, prompt=inp)
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    description = llava.chat(image, inp, None, max_new_tokens=300)
     print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
     print('Output description:\n', description)
-
+    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+    
     is_question = qd.is_question(inp)
-    ans = llava.chat(image, prompt=inp, is_question=is_question)
+    ans = llava.chat(image, inp=inp, is_question=is_question)
     print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-    print(ans)
+    if is_question:
+        print(ans[0])
+        print(ans[1])
+    else:
+        print(ans)
+    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+    
     if not is_question:
         question = f"{ans}"
         template = "what are the main objects explicitly described in this: {question} and related to " + f"{inp}"
         ans = llama.chat(question, template)
         print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('llama:\n' + ans)
         print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-        print('llama: ' + ans)
+        
 
     # Image segmentation with langsam
     bbox_sam = BoundingBoxSAM(token=token)
-    bbox_sam.process_image(image_url, image_path, ans, output_path='output.jpg')
+    if is_question:
+        bbox_sam.process_image(image_url, image_path, ans[0], output_path=f'output_images/{image_name}')
+    else:
+        bbox_sam.process_image(image_url, image_path, ans, output_path=f'output_images/{image_name}')
 
 # Example usage
 if __name__ == "__main__":
     # use ur token here
-    token = "token" 
+    token = "3ef4060368982c4cb1fb2487d584199c" 
     while(True):
         inp = input('Prompt: ')
         image_path = input('Image path: ')
         image_url = input('url: ')
+        if image_url.endswith('0'):
+            image_url = image_url[:len(image_url)-1] + '1'
         main(inp, image_path, image_url)
--- models/llava.py
+++ models/llava.py
@@ -3,7 +3,7 @@ from PIL import Image
 from transformers import AutoProcessor, LlavaOnevisionForConditionalGeneration
 
 class Llava:
-    def __init__(self, model_name="llava-hf/llava-onevision-qwen2-0.5b-si-hf", device=0):
+    def __init__(self, model_name="llava-hf/llava-onevision-qwen2-0.5b-ov-hf", device=0):
         self.model_name = model_name
         self.device = device
         self.model, self.processor = self.load_model_and_processor()
@@ -19,7 +19,7 @@ class Llava:
         processor = AutoProcessor.from_pretrained(self.model_name)
         return model, processor
 
-    def chat(self, image, prompt, is_question = None, max_new_tokens=300):
+    def chat(self, image, inp, is_question = None, max_new_tokens=77, ans=False):
         """Generates a detailed description of an image."""
         
         conversation = [
@@ -40,7 +40,7 @@ class Llava:
                 "role": "user",
                 "content": [
                     {"type": "image"},
-                    {"type": "text", "text": f"describe the {prompt} shortly"},
+                    {"type": "text", "text": f"describe the {inp} shortly"},
                 ],
             },
         ]
@@ -50,10 +50,41 @@ class Llava:
                 "role": "user",
                 "content": [
                     {"type": "image"},
-                    {"type": "text", "text": f"Answer this as short as possible: {prompt}"},
+                    {"type": "text", "text": f"Answer this as short as possible: {inp}"},
                 ],
             },
         ]
+
+        conversation_answer = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image"},
+                    {"type": "text", "text": f"Answer this question and explain the reason: {inp}"},
+                ],
+            },
+        ]
+        if ans:
+            prompt = self.processor.apply_chat_template(conversation_answer, add_generation_prompt=True)
+            inputs = self.processor(
+                images=image, text=prompt, return_tensors="pt"
+            ).to(self.device, torch.float16)
+
+            output = self.model.generate(
+                **inputs, 
+                max_new_tokens=max_new_tokens, 
+                do_sample=True,
+                temperature=0.1,
+                top_k=30,
+                top_p=0.9
+            )
+            output = self.processor.decode(
+                output[0][2:], skip_special_tokens=True
+            )
+            output = str(output).split('assistant')[1]
+            if output.startswith('\n'):
+                output = output[1:]
+            return output
         if is_question == None:
             prompt = self.processor.apply_chat_template(conversation, add_generation_prompt=True)
         elif is_question:
@@ -78,13 +109,16 @@ class Llava:
         output = str(output).split('assistant')[1]
         if output.startswith('\n'):
             output = output[1:]
+        
+        if is_question:
+            return output, self.chat(image, inp, is_question, ans=True)
         return output
 
 # Usage
 if __name__ == "__main__":
-    image = Image.open('dog.jpg').convert('RGB')
+    image = Image.open('trump.png').convert('RGB')
     llava = Llava()
-    prompt = "grass"
+    prompt = "trump"
     print(llava.chat(image, prompt, is_question=False))
     prompt = "what's the thing may bark"
     print(llava.chat(image, prompt, is_question=True))
